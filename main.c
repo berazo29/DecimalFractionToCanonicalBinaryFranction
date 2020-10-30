e8929905b3b3fdfd68be2d9204e69f302020aa45
Bryan Erazo <br@MacBook-Pro.local>
2020-10-30 00:21:40 -0400

    added decimal to binary function, and linked-list utility functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // Data Structure
 struct Node{
@@ -41,11 +42,79 @@ void freeNodes(struct Node **head){
     //printf("Free completed\n");
 }
 
-// Binary Transformations
+void reverseLinkedList(struct Node **head){
+    struct Node *ptr = (*head);
+    struct Node *next = NULL;
+    struct Node *prev = NULL;
 
+    while (ptr != NULL){
+        next = ptr->next;
+        ptr->next = prev;
+        prev = ptr;
+        ptr = next;
+    }
+    (*head) = prev;
+
+}
+int linkedListNodeCounter(struct Node *head){
+    int counter = 0;
+
+    if ( head == NULL){
+        return counter;
+    }
+
+    while ( head != NULL){
+        counter++;
+        head = head->next;
+    }
+    return counter;
+}
+
+// Binary Transformations
+int decimalToBinary(unsigned long decimal, struct Node **head){
+
+    int counter = 0;
+    unsigned long tmp = decimal;
+    unsigned char bit;
+
+    if (decimal < 0){
+        return -1;
+    }
+    if ( decimal == 0 ){
+        insertNode(head,0);
+        return 1;
+    }
+
+    while (tmp != 0){
+        bit = tmp % 2;
+        tmp = tmp/2;
+        insertNode(head,bit);
+        counter++;
+    }
+    return counter;
+}
+long int binaryToDecimal(struct Node *head){
+
+    int numberNodes = linkedListNodeCounter(head);
+    long int result=0;
+
+    while (head != NULL){
+        if ( head->bit == 1){
+            result += (long int)pow(2,numberNodes-1);
+        }
+        numberNodes--;
+        head = head->next;
+    }
+    return result;
+}
 int main() {
 
+    struct Node *ptr = NULL;
+    decimalToBinary(1,&ptr);
+    printLinkedList(ptr);
+    printf("Linked list size:%d\n", linkedListNodeCounter(ptr));
+    printf("Decimal:%li\n", binaryToDecimal(ptr));
 
-
+    freeNodes(&ptr);
     return 0;
 }
